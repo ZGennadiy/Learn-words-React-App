4b91015d7db5c38ebbe975db8e4622d9ddc9f254
ZGennadiy <gennadiy.zinchenko@gmail.com>
2020-05-16 15:19:28 +0300

    homework 3

--- src/index.js
+++ src/index.js
@@ -1,51 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from './App';
+
+import 'normalize.css';
 import './index.css';
-import HeaderBlock from './components/HeaderBlock/indexA.js';
 
-// const AppList = () => {
-//   const items = ['item1', 'item2', 'another item', 'and another one'];
-//   const firstItem = <li>first item</li>;
-
-//   const isAuth = false;
-
-//   return (
-//     <ul>
-//       { isAuth ? firstItem : null }
-//       { items.map(item => <li>{item}</li>) }
-//       <li>{ items[0] }</li>
-//       <li>{ items[1] }</li>
-//     </ul>
-//   )
-// };
-
-// const AppHeader = () => {
-//   return (
-//     <h1 className='header'>Header</h1>
-//   )
-// };
-
-// const AppInput = () => {
-//   const placeHolder = 'Type: text';
-//   return (
-//     <label htmlFor='search'>
-//       <input id='search' placeholder={ placeHolder }/>
-//     </label>
-//   )
-// };
-
-const App = () => {
-  return (
-    <>
-      <HeaderBlock />
-      {/* <AppHeader />
-      <AppInput />
-      <AppList />
-      <AppHeader />
-      <AppList /> */}
-    </>
-  )
-};
 
 
 ReactDom.render(<App />, document.getElementById('root'));
