6c6e2f550efc98ff8a1e5d4fd9d7db0e213d0735
ZGennadiy <gennadiy.zinchenko@gmail.com>
2020-09-03 08:43:06 +0300

    gh-pages

---
+++ precache-manifest.46437bcd4adcc097ee0bb0f64dc4db14.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43581789613cc8756c9acbb5defe6c4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb49b0c4ef758dc4fe1e",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f1a6358834a15d5192f7",
+    "url": "/static/css/main.02e257ff.chunk.css"
+  },
+  {
+    "revision": "cb49b0c4ef758dc4fe1e",
+    "url": "/static/js/2.99ec8a2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99ec8a2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a6358834a15d5192f7",
+    "url": "/static/js/main.a1c4679b.chunk.js"
+  },
+  {
+    "revision": "af8f8a401cba4cb0a8d2",
+    "url": "/static/js/runtime-main.21f82e15.js"
+  },
+  {
+    "revision": "898d65d07b3ff005f2afa1f917b5f6da",
+    "url": "/static/media/background.898d65d0.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
